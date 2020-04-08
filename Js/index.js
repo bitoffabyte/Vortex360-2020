4d0c2951d8c53b66f479b6cff0cd4367fcf4fc2c
pulkitmahajan23 <pulkitmahajan1998@gmail.com>
2020-04-09 02:19:46 +0530

    Changes made to timer

--- Js/index.js
+++ Js/index.js
@@ -43,6 +43,12 @@ function updateTimer() {
     if (s<10) {
       s="0"+s;
     }
+    if (m<10) {
+      m="0"+m;
+    }
+    if (h<10) {
+      h="0"+h;
+    }
   
     document.getElementById("timer")
       .innerHTML =
