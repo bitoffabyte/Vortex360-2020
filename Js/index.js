62828e190f1d23a12c43a0c182ae48e17b050b8b
kodekandy <60323313+kodekandy@users.noreply.github.com>
2020-05-05 18:20:04 +0530

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -180,7 +180,7 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
   });
-/*devfolio registration button*/
+/*devfolio registration button
 document.addEventListener('DOMContentLoaded', function () {
   let devfolioOptions = {
       buttonSelector: '#devfolio-apply-now',
@@ -200,4 +200,26 @@ document.addEventListener('DOMContentLoaded', function () {
           window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
       });
   }
-});
+});*/
+//pop-up
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  img.style.display= "none";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() { 
+  modal.style.display = "none";
+  img.style.display="block";
+}
