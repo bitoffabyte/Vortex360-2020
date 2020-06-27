4fa78feb06a5c620c4d717ff4f4cab4b81859d6a
R Narayan <nr.rnarayan@gmail.com>
2020-06-27 21:11:00 +0530

    carousal

--- Js/index.js
+++ Js/index.js
@@ -16,6 +16,64 @@ $("#mainListDiv").fadeIn();
 });
 
 
+var slideIndex = 0;
+var slideIndex2 = 0;
+var slideIndex3 = 0;
+
+showSlides();
+showSlides2();
+showSlides3();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function showSlides2() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides2");
+    var dots = document.getElementsByClassName("dot2");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides.length) {slideIndex2 = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2-1].style.display = "block";  
+    dots[slideIndex2-1].className += " active";
+    setTimeout(showSlides2, 2000); // Change image every 2 seconds
+  }
+
+  function showSlides3() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides3");
+    var dots = document.getElementsByClassName("dot3");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex3++;
+    if (slideIndex3 > slides.length) {slideIndex3 = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex3-1].style.display = "block";  
+    dots[slideIndex3-1].className += " active";
+    setTimeout(showSlides3, 2000); // Change image every 2 seconds
+  }
 //faq
 const toggles = document.querySelectorAll('.faq-toggle');
 
@@ -240,3 +298,5 @@ span11.onclick = function() {
   modal1.style.display = "none";
   console.log("sds")
 }
+
+
